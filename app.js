e29402539479eaec2269edb532e933a35a03bf4b
DarshaKarunakaran <sk3526@njit.edu>
2025-01-03 19:34:53 -0500

    working metronome with changed header setup

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-import Timer from './timer.js';
+var metronome = new Metronome();
+var tempo = document.getElementById('tempo');
+tempo.textContent = metronome.tempo;
 
-// Variables for the UI elements and the audio file
-const tempoSlider = document.querySelector(".tempoSlider");
-const tempoDisplay = document.querySelector(".tempoValue");
-const click1 = new Audio('click1.wav');
+var playPauseIcon = document.getElementById('play-pause-icon');
 
-// Set an initial BPM value
-let bpm = 120; // Default BPM
+var playButton = document.getElementById('play-button');
+playButton.addEventListener('click', function() {
+    metronome.startStop();
 
-// Initialize the metronome
-const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
+    if (metronome.isRunning) {
+        playPauseIcon.className = 'pause';
+    }
+    else {
+        playPauseIcon.className = 'play';
+    }
 
-// Update BPM when slider is adjusted
-tempoSlider.addEventListener('input', () => {
-    bpm = tempoSlider.value;  // Update bpm from the slider
-    tempoDisplay.textContent = bpm;  // Update tempo display
-    metronome.timeInterval = 60000 / bpm;  // Update the metronome interval based on BPM
-    metronome.stop();  // Stop the previous timer
-    metronome.start();  // Start the timer again with the new interval
 });
 
-// Function to play the click sound
-function playClick() {
-    click1.play();
+var tempoChangeButtons = document.getElementsByClassName('tempo-change');
+for (var i = 0; i < tempoChangeButtons.length; i++) {
+    tempoChangeButtons[i].addEventListener('click', function() {
+        metronome.tempo += parseInt(this.dataset.change);
+        tempo.textContent = metronome.tempo;
+    });
 }
-
-// Start the timer for the first time
-metronome.start();
