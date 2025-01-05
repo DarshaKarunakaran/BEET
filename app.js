d762713c593fa17e069b6528b7f56e5c43f1bbef
DarshaKarunakaran <sk3526@njit.edu>
2025-01-04 21:12:11 -0500

    javascript added to change beat button colors when clicked

--- app.js
+++ app.js
@@ -17,10 +17,28 @@ playButton.addEventListener('click', function() {
 
 });
 
+var button = document.getElementsByClassName('beat-box');
+        button.addEventListener('click',
+            () => {
+                button.style.backgroundColor = 
+                    "green";
+            });
+
 var tempoChangeButtons = document.getElementsByClassName('tempo-change');
 for (var i = 0; i < tempoChangeButtons.length; i++) {
     tempoChangeButtons[i].addEventListener('click', function() {
         metronome.tempo += parseInt(this.dataset.change);
         tempo.textContent = metronome.tempo;
     });
+}
+function changeColor(button) {
+    const colors = ['#ff9aa2', 'pink'];
+
+    let colorIndex = button.dataset.colorIndex ? parseInt(button.dataset.colorIndex) : 0;
+
+    colorIndex = (colorIndex + 1) % colors.length;
+
+    button.style.background = colors[colorIndex];
+
+    button.dataset.colorIndex = colorIndex;
 }
